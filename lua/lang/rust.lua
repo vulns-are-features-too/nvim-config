c8ef847c9ca2c19926298a844ddb37fa243b82db
ChocolateOverflow <57666296+ChocolateOverflow@users.noreply.github.com>
2024-04-06 22:11:36 +0700

    removed lang/rust.lua

--- lua/lang/rust.lua
+++
@@ -1,3 +0,0 @@
-vim.b.dispatch = 'cargo build'
-vim.opt_local.tags = './rusty-tags.vi;/,$RUST_SRC_PATH/rusty-tags.vi'
-vim.cmd([[silent! exec "!rusty-tags vi --quiet --start-dir=" . expand('%:p:h') . "&" | redraw!]])
