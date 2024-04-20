36307146233402a436cea382b6f92bd20aa825e4
ChocolateOverflow <57666296+ChocolateOverflow@users.noreply.github.com>
2024-04-20 10:19:01 +0700

    plugins: removed better-escape

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -103,7 +103,6 @@ local plugins = {
   },
   { 'wellle/targets.vim' },
   -- Misc
-  { 'max397574/better-escape.nvim', config = c('better-escape') },
   { 'kevinhwang91/nvim-hlslens', config = c('hlslens') },
 
   -- telescope
