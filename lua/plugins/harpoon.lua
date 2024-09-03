dad61cfac0012b5b84f0b75d01eb18d6863c00f1
ChocolateOverflow <57666296+ChocolateOverflow@users.noreply.github.com>
2024-09-03 10:48:52 +0700

    plugins: migrated to harpoon2 (doesn't work)

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,17 +1,17 @@
-require('harpoon').setup()
-local ui = require('harpoon.ui')
+local harpoon = require('harpoon')
+
+harpoon:setup()
+
 local function nmap(key, target, desc) vim.keymap.set('n', key, target, { desc = desc }) end
 
 local function nav(file)
-  return function()
-    ui.nav_file(file)
-  end
+  return function() harpoon:list():select(file) end
 end
 
-nmap('mm', require('harpoon.mark').add_file, 'Harpoon add file')
-nmap('m/', ui.toggle_quick_menu, 'Harpoon menu')
-nmap('m,', ui.nav_prev, 'Harpoon navigate back')
-nmap('m.', ui.nav_next, 'Harpoon navigate next')
+nmap('mm', function() harpoon:list():add() end, 'Harpoon add file')
+nmap('m/', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, 'Harpoon menu')
+nmap('m,', function() harpoon:list():prev() end, 'Harpoon navigate back')
+nmap('m.', function() harpoon:list():next() end, 'Harpoon navigate next')
 
 -- file mappings based on my split keyboard layout
 nmap('m1', nav(1), 'Harpoon nav file 1')
@@ -24,3 +24,25 @@ nmap('m9', nav(2), 'Harpoon nav file 2')
 nmap('m8', nav(3), 'Harpoon nav file 3')
 nmap('m7', nav(4), 'Harpoon nav file 4')
 nmap('m6', nav(5), 'Harpoon nav file 5')
+
+-- basic telescope configuration
+local conf = require('telescope.config').values
+local function toggle_telescope(harpoon_files)
+  local file_paths = {}
+  for _, item in ipairs(harpoon_files.items) do
+    table.insert(file_paths, item.value)
+  end
+
+  require('telescope.pickers')
+    .new({}, {
+      prompt_title = 'Harpoon',
+      finder = require('telescope.finders').new_table({
+        results = file_paths,
+      }),
+      previewer = conf.file_previewer({}),
+      sorter = conf.generic_sorter({}),
+    })
+    :find()
+end
+
+nmap('<space>fm', function() toggle_telescope(harpoon:list()) end, 'Telescope harpoon marks')
--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -78,6 +78,7 @@ local plugins = {
   -- Moving
   {
     'ThePrimeagen/harpoon',
+    branch = "harpoon2",
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = c('harpoon'),
   },
