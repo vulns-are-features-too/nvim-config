ab36d822d4c36c416947e64070715d4787c2fb85
vulns-are-features-too <57666296+vulns-are-features-too@users.noreply.github.com>
2025-08-17 08:54:26 +0700

    plugin(leap.nvim): update conf

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -2,6 +2,6 @@ local function nmap(key, target) vim.keymap.set('n', key, target, { silent = tru
 
 require('leap').add_default_mappings()
 
-nmap('s', '<Plug>(leap-forward-to)')
-nmap('S', '<Plug>(leap-backward-to)')
+nmap('s', '<Plug>(leap-forward)')
+nmap('S', '<Plug>(leap-backward)')
 nmap('<space>g<space>', '<Plug>(leap-cross-window)')
