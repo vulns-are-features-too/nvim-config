46aa39d15c24c410b75b1d888d7244ed905e022d
vulns-are-features-too <57666296+vulns-are-features-too@users.noreply.github.com>
2025-10-26 15:09:53 +0700

    plugin(leap): remove deprecated add_default_mappings()

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -1,7 +1,5 @@
 local function nmap(key, target) vim.keymap.set('n', key, target, { silent = true }) end
 
-require('leap').add_default_mappings()
-
 nmap('s', '<Plug>(leap-forward)')
 nmap('S', '<Plug>(leap-backward)')
 nmap('<space>g<space>', '<Plug>(leap-cross-window)')
